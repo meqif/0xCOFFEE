179b21a8658b738efdb7583fa2fa00e0450f2537
Ricardo Martins <ricardo@scarybox.net>
2009-12-23 18:37:58 +0000

    Disabling generator for now.

--- lib/coffee.rb
+++ lib/coffee.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "treetop"
 
 #require "coffee/runtime"
-require "coffee/generator"
+#require "coffee/generator"
 require "coffee/nodes"
 
 if File.file?(File.dirname(__FILE__) + "/coffee/grammar.rb")
