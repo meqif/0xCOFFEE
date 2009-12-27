9b7b26281085c838e36f0b0707f8fd4ebffa1083
Ricardo Martins <ricardo@scarybox.net>
2009-12-27 23:16:04 +0000

    Corrected whitespace.

--- test/interpreter_test.rb
+++ test/interpreter_test.rb
@@ -25,50 +25,50 @@ end
 
 class Interpreter < Test::Unit::TestCase
 
-    attr_reader :parser
+  attr_reader :parser
 
-    def setup
-        @parser = CoffeeParser.new
-    end
+  def setup
+    @parser = CoffeeParser.new
+  end
 
-    def test_identity
-        root = parser.parse('1')
-        assert_equal(1, root.evaluate)
-    end
+  def test_identity
+    root = parser.parse('1')
+    assert_equal(1, root.evaluate)
+  end
 
-    def test_addition
-        root = parser.parse('1+1')
-        assert_equal(2, root.evaluate)
+  def test_addition
+    root = parser.parse('1+1')
+    assert_equal(2, root.evaluate)
 
-        root = parser.parse('1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1')
-        assert_equal(18, root.evaluate)
+    root = parser.parse('1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1')
+    assert_equal(18, root.evaluate)
 
-        root = parser.parse('0 + 0 + 1 + 2 + 3 + 5 + 8 + 13 + 21')
-        assert_equal(53, root.evaluate)
+    root = parser.parse('0 + 0 + 1 + 2 + 3 + 5 + 8 + 13 + 21')
+    assert_equal(53, root.evaluate)
 
-        root = parser.parse('2 + (-3) * 2')
-        assert_equal(-4, root.evaluate)
-    end
+    root = parser.parse('2 + (-3) * 2')
+    assert_equal(-4, root.evaluate)
+  end
 
-    def test_addition_fail
-        assert_nil parser.parse('1+1+1++1')
-    end
+  def test_addition_fail
+    assert_nil parser.parse('1+1+1++1')
+  end
 
-    def test_multiplication
-      root = parser.parse('2*2')
-      assert_equal(4, root.evaluate)
+  def test_multiplication
+    root = parser.parse('2*2')
+    assert_equal(4, root.evaluate)
 
-      rootA = parser.parse('5*4*3*2*1')
-      rootB = parser.parse('1*2*3*4*5')
-      assert_equal(rootA.evaluate, rootB.evaluate)
-    end
+    rootA = parser.parse('5*4*3*2*1')
+    rootB = parser.parse('1*2*3*4*5')
+    assert_equal(rootA.evaluate, rootB.evaluate)
+  end
 
-    def test_priority
-      root = parser.parse('1+2*2')
-      assert_equal(5, root.evaluate)
+  def test_priority
+    root = parser.parse('1+2*2')
+    assert_equal(5, root.evaluate)
 
-      root = parser.parse('(1+2)*2')
-      assert_equal(6, root.evaluate)
-    end
+    root = parser.parse('(1+2)*2')
+    assert_equal(6, root.evaluate)
+  end
 
 end
